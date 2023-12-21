a84385e537c8906ac31b9a0d63c2cb8bdc44d751
Josemar Damasceno <josemardm@gmail.com>
2023-12-21 15:58:47 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from io import StringIO  # Importe StringIO da biblioteca io
+
+
+def buscaTabelaResultado(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    # Encontre a tabela
+    tabela = soup.findAll('table')
+    df = pd.read_html(StringIO(str(tabela)))[0]
+    array_resultado = df.to_numpy()  # Converte o DataFrame para um array NumPy
+    return array_resultado.tolist()  # Converte o array NumPy para uma lista Python
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+
+@app.route('/api/v1/resultado', methods=['GET'])
+def resultado():
+    dados = buscaTabelaResultado('https://www.ojogodobicho.com/deu_no_poste.htm')
+    return jsonify(dados)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
