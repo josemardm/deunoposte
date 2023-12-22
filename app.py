767965d8947cc43ad9b6d0a8201c0fbb2f949f8d
Josemar Damasceno <josemardm@gmail.com>
2023-12-22 18:56:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,36 @@ def buscaTabelaResultado(url):
     if tabela:
         df = pd.read_html(str(tabela))[0]
         dados = df.to_dict(orient='records')
-        return dados
+
+        dadosPTM = {}
+        dadosPT = {}
+        dadosPTV = {}
+        dadosPTN = {}
+        dadosCOR = {}
+
+        for index in range(len(dados)):  # Alteração aqui
+
+            dadosPTM[index] = dados[index]['PTM']
+            dadosPT[index] = dados[index]['PT']
+            dadosPTV[index] = dados[index]['PTV']
+            dadosPTN[index] = dados[index]['PTN']
+            dadosCOR[index] = dados[index]['COR']
+
+        resultado = [dadosPTM, dadosPT, dadosPTV, dadosPTN, dadosCOR]
+
+        return resultado
     else:
         return {"error": "Nenhuma tabela encontrada na página"}
 
+
 app = Flask(__name__)
+
 
 @app.route('/api/v1/resultado', methods=['GET'])
 def resultado():
     dados = buscaTabelaResultado('https://www.ojogodobicho.com/deu_no_poste.htm')
     return jsonify(dados)
 
-# A remoção de app.run() é necessária quando usando Gunicorn
 
 if __name__ == '__main__':
     app.run()
